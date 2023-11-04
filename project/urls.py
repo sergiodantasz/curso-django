7f92849f366c5d485905212630b8d9b5657e10d4
Sérgio Dantas <sergiodnts828@gmail.com>
2023-11-04 18:33:08 -0300

    criado o app recipes e movidas as URLs

--- project/urls.py
+++ project/urls.py
@@ -15,15 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
-from django.urls import path
-
-
-def my_view(request):
-    return HttpResponse('That is a HTTP response.')
-
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('about/', my_view)  # Recebe a URL e a view.
+    path('', include('recipes.urls')),
 ]
---
+++ recipes/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return HttpResponse('HOME')
+
+
+def about(request):
+    return HttpResponse('ABOUT')
+
+
+def contact(request):
+    return HttpResponse('CONTACT')
