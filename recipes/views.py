0317e71b124eda2d62a673f6c2852c4facd74477
Sérgio Dantas <sergiodnts828@gmail.com>
2023-11-06 19:52:55 -0300

    adicionadas algumas configurações dinâmicas do site e geração de dados fictícios

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(
+        request,
+        'recipes/pages/home.html',
+        {
+            'recipes': [make_recipe() for _ in range(9)],
+        }
+    )
+
+
+def recipe(request, id):
+    return render(
+        request,
+        'recipes/pages/recipe.html',
+        {
+            'recipe': make_recipe(),
+        }
+    )
