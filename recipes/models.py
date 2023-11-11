c5ce6d4a87f75b57017c8759d9138b29ce17e456
Sérgio Dantas <sergiodnts828@gmail.com>
2023-11-11 13:38:31 -0300

    criada a view category e adicionadas algumas validações nos templates

--- recipes/models.py
+++ recipes/models.py
@@ -22,8 +22,8 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to=r'recipes/covers/%Y/%m/%d/')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    cover = models.ImageField(upload_to=r'recipes/covers/%Y/%m/%d/', blank=True, default='')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL,blank=True, default=None, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
--- recipes/views.py
+++ recipes/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 
 from utils.recipes.factory import make_recipe
 
+from . import models
+
 
 def home(request):
+    recipes = models.Recipe.objects.filter(is_published=True).order_by('-id')
     return render(
         request,
         'recipes/pages/home.html',
         {
-            'recipes': [make_recipe() for _ in range(9)],
+            'recipes': recipes,
         }
     )
 
@@ -20,5 +23,16 @@ def recipe(request, id):
         {
             'recipe': make_recipe(),
             'is_detail_page': True,
+        }
+    )
+
+
+def category(request, category_id):
+    recipes = models.Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    return render(
+        request,
+        'recipes/pages/category.html',
+        {
+            'recipes': recipes,
         }
     )
