11ba689eb090bbf82cdf4311592e77077b801a88
Sérgio Dantas <sergiodnts828@gmail.com>
2023-11-28 10:46:08 -0300

    iniciada a configuração de testes

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class RecipeURLsTest(TestCase):
+    def test_if_pytest_is_ok(self):
+        variavel = 10
+        print('testando o pytest')
+        assert 1 == 1, '1 é igual a 1'
