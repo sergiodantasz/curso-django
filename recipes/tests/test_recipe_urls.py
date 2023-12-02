62733207d1990bc1485aa4264ccd24942a53703d
Sérgio Dantas <sergiodnts828@gmail.com>
2023-12-02 14:32:00 -0300

    criados mais testes

--- recipes/tests/test_recipe_urls.py
+++ recipes/tests/test_recipe_urls.py
@@ -10,17 +10,13 @@ class RecipeURLsTest(TestCase):
     def test_recipe_category_url_is_correct(self):
         url = reverse(
             'recipes:category',
-            kwargs={
-                'category_id': 1,
-            }
+            kwargs={'category_id': 1}
         )
         self.assertEqual(url, '/recipes/category/1/')
 
     def test_recipe_detail_url_is_correct(self):
         url = reverse(
             'recipes:recipe',
-            kwargs={
-                'id': 1,
-            }
+            kwargs={'id': 1}
         )
         self.assertEqual(url, '/recipes/1/')
