332cfca081765197a1e2f00e4c749b20a46e038f
Sérgio Dantas <sergiodnts828@gmail.com>
2023-12-03 23:21:02 -0300

    adicionados filtros na view search

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
@@ -48,10 +49,18 @@ def search(request):
     search_term = request.GET.get('q', '').strip()
     if not search_term:
         raise Http404()
+    recipes = models.Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term)
+        ),
+        is_published=True
+    ).order_by('-id')
     return render(
         request,
         'recipes/pages/search.html',
         {
             'page_title': f'Search for "{search_term}"',
+            'recipes': recipes
         }
     )
