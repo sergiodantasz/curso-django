84c1c302ea22e46e698992e80d63d5ff83242ff7
Sérgio Dantas <sergiodnts828@gmail.com>
2023-12-03 22:51:12 -0300

    configurado o título da página search

--- recipes/views.py
+++ recipes/views.py
@@ -45,10 +45,13 @@ def category(request, category_id):
 
 
 def search(request):
-    search_term = request.GET.get('q')
+    search_term = request.GET.get('q', '').strip()
     if not search_term:
         raise Http404()
     return render(
         request,
-        'recipes/pages/search.html'
+        'recipes/pages/search.html',
+        {
+            'page_title': f'Search for "{search_term}"',
+        }
     )
